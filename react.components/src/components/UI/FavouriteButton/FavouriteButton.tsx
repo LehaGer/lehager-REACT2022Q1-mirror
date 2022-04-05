3b779fb8b23828538d806c7c6fa24d00ee16630e
Darya Nalivayko <dashanalivayko@gmail.com>
2022-04-06 01:26:34 +0300

    add favourites

--- react.components/src/components/UI/FavouriteButton/FavouriteButton.tsx
+++ react.components/src/components/UI/FavouriteButton/FavouriteButton.tsx
@@ -3,14 +3,26 @@ import { IFavouriteButtonProps } from '../../../types/interfaces';
 import ItemStyles from './FavouriteButton.module.css';
 import favouriteIcon from '../../../asserts/svg/favourite-icon.svg';
 
-class FavouriteButton extends React.Component<IFavouriteButtonProps> {
+export interface IFavouriteButton {
+  isLiked: boolean;
+}
+class FavouriteButton extends React.Component<IFavouriteButtonProps, IFavouriteButton> {
   constructor(props: IFavouriteButtonProps) {
     super(props);
+
+    this.state = {
+      isLiked: false,
+    };
   }
+
   render() {
+    const isLiked = this.state.isLiked;
     return (
-      <button className={ItemStyles.fvrBtn}>
-        {/*<img src={favouriteIcon} alt="like-btn" />*/}
+      <button
+        className={ItemStyles.fvrBtn}
+        style={{ backgroundColor: isLiked ? 'red' : 'rgba(0,0,0,0.1)' }}
+        onClick={() => this.setState({ isLiked: !isLiked })}
+      >
       </button>
     );
   }
