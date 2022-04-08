8de228fbc1636cc2ba1fd39dfc4a5e1f68ed2611
Leha Gerasimchuk <leha.ger97@mail.ru>
2022-04-08 21:19:05 +0300

    refactor: refactored code style according to Prettier rules

--- react.components/src/components/UI/FavouriteButton/FavouriteButton.tsx
+++ react.components/src/components/UI/FavouriteButton/FavouriteButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { IFavouriteButtonProps } from '../../../types/interfaces';
 import ItemStyles from './FavouriteButton.module.css';
-import favouriteIcon from '../../../asserts/svg/favourite-icon.svg';
 
 export interface IFavouriteButton {
   isLiked: boolean;
@@ -22,8 +21,7 @@ class FavouriteButton extends React.Component<IFavouriteButtonProps, IFavouriteB
         className={ItemStyles.fvrBtn}
         style={{ backgroundColor: isLiked ? 'red' : 'rgba(0,0,0,0.1)' }}
         onClick={() => this.setState({ isLiked: !isLiked })}
-      >
-      </button>
+      />
     );
   }
 }
